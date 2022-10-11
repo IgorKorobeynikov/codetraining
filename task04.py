45d6fcaf28f2c0afdef27fa510b2506a9ec2f463
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-10-11 21:09:39 +0300

    Update task04.py

--- task04.py
+++ task04.py
@@ -1,6 +1,7 @@
 """
 https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/python
 """
+# My solution:
 def find_uniq(arr):
     """
     Time complexity: O(n)
@@ -12,3 +13,11 @@ def find_uniq(arr):
         
     for element in arr:
         if element != n: return element
+
+# Solution by eqlion https://www.codewars.com/kata/reviews/5941972edacd78ae0a00013c/groups/5942ce6bb4513a72e4000218
+def find_uniq(arr):
+    """
+    Summary time complexity: O(2*n)
+    """
+    a, b = set(arr) # O(n)
+    return a if arr.count(a) == 1 else b # O(n)
