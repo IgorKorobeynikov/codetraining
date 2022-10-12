61ad48fbceb6b2071e603b294a442ffdd59e3a18
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-10-12 20:07:05 +0300

    Create task06.py

---
+++ task06.py
@@ -0,0 +1,11 @@
+"""
+https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/python
+"""
+# My clumsy solution:
+from collections import Counter
+
+def scramble(s1, s2):
+    s1 = Counter(s1)
+    for c in Counter(s2):
+        if s1[c] < s2[c]: return False
+    return True
