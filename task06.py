41a319883ab0c813f06e11902cc1b6d5f7e7694e
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-10-12 20:08:31 +0300

    fixed bug in solution

--- task06.py
+++ task06.py
@@ -6,6 +6,7 @@ from collections import Counter
 
 def scramble(s1, s2):
     s1 = Counter(s1)
-    for c in Counter(s2):
+    s2 = Counter(s2)
+    for c in s2:
         if s1[c] < s2[c]: return False
     return True
