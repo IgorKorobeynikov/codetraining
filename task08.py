6f66aaed5d04b4acac194a9b61fbb96ae6d7efe9
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-10-17 21:06:10 +0300

    Create task08.py

---
+++ task08.py
@@ -0,0 +1,25 @@
+"""
+https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+"""
+def zero(f = None): return 0 if not f else f(0)
+def one(f = None): return 1 if not f else f(1)
+def two(f = None): return 2 if not f else f(2)
+def three(f = None): return 3 if not f else f(3)
+def four(f = None): return 4 if not f else f(4)
+def five(f = None): return 5 if not f else f(5)
+def six(f = None): return 6 if not f else f(6)
+def seven(f = None): return 7 if not f else f(7)
+def eight(f = None): return 8 if not f else f(8)
+def nine(f = None): return 9 if not f else f(9)
+
+def plus(y): return lambda x: x+y
+def minus(y): return lambda x: x-y
+def times(y): return lambda  x: x*y
+def divided_by(y): return lambda  x: x//y
+
+# Unrolling:
+seven(times(five()))
+seven(times(5))
+seven(lambda x: x*5)
+7*5
+35
