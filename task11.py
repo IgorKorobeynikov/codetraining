c83a8df24c3fc33e849ce6594009cfd919dd9ff0
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-12-05 20:17:05 +0300

    Create task11.py

---
+++ task11.py
@@ -0,0 +1,9 @@
+"""
+https://www.codewars.com/kata/52774a314c2333f0a7000688/python
+"""
+
+def valid_parentheses(string: str) -> bool:
+    string = str().join(filter(lambda c: 40 <= ord(c) <= 41 , string))
+    try: compile(string, '<string>', 'exec')
+    except (SyntaxError, SyntaxWarning): return False
+    else: return True
