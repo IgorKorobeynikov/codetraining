3de040238574a3e7932f4f32f8eda0ff26e943df
LINUX_ADMIN <73735838+linux-admin0001@users.noreply.github.com>
2022-12-06 20:32:36 +0300

    `Snail Sort` 4kyu kata (not implemented yet)

---
+++ task14.py
@@ -0,0 +1,12 @@
+"""
+https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
+"""
+
+from typing import List, Any, TypeVar
+
+T = TypeVar('T')
+
+def snail(snail_map: List[List[T]]) -> List[T]:
+    if not len(snail_map): return []
+
+    raise NotImplementedError # Not implemented yet
